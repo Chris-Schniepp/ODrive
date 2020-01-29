1f1fa3ac3c63978d50039389c801fd8d86f38df6
Chris Schniepp <chrisschniepp@dpengineering.org>
2020-01-29 14:51:29 -0800

    Using Trajectory mode to move ODrive to specified location

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ axis = ODrive_Ease_Lib.ODrive_Axis(OD.axis1)
 if __name__ == '__main__':
     axis.clear_errors()
     axis.calibrate()
-    axis.home_with_vel(-6000, -112844.0)
+    axis.home_with_vel(-10000, -112844.0)
     print(axis.zero)
     print(axis.get_pos())
     middle = (0 - axis.get_pos())/2
@@ -26,12 +26,13 @@ if __name__ == '__main__':
     # left_end = right_end ^ left_end
     # right_end = right_end ^ left_end
 
-    axis.set_vel_limit(27000)
+    axis.set_vel_limit(70000)
 
     # axis.set_pos_gain(30)
     # axis.set_vel_gain(.0002)
 
     switch = True
+    tracking = False
 
     # axis.set_curr_limit(20)
     # axis.set_current(15)
@@ -39,19 +40,20 @@ if __name__ == '__main__':
     try:
         while True:
 
+            tracking = False
             pos = axis.get_pos()
 
-            if pos > -(full_length-4000) and joystick.get_axis('x') > .15:
+            if pos > -(full_length-6000) and joystick.get_axis('x') > .15 and not tracking:
                 if switch:
                     print("right")
                     switch = False
-                axis.set_vel(-22000*joystick.get_axis('x'))
+                axis.set_vel(-65000*joystick.get_axis('x'))
                 # axis.set_pos(pos-(1000*joystick.get_axis('x')))
-            elif pos < -4000 and joystick.get_axis('x') < -.15:
+            elif pos < -6000 and joystick.get_axis('x') < -.15 and not tracking:
                 if not switch:
                     print("left")
                     switch = True
-                axis.set_vel(22000*-joystick.get_axis('x'))
+                axis.set_vel(65000*-joystick.get_axis('x'))
                 # axis.set_pos(pos + (1000*-joystick.get_axis('x')))
             else:
                 axis.set_vel(0)
@@ -65,6 +67,7 @@ if __name__ == '__main__':
 
             if joystick.button_combo_check([3]):
                 print("Oof")
+                tracking = True
                 while abs(axis.get_pos() - -middle) >= 50:
                     axis.set_pos_trap(-middle)
                     axis.get_pos()
