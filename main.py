2e13946a8f50295236781b343ba89377f86053c7
Chris Schniepp <chrisschniepp@dpengineering.org>
2020-01-29 14:26:35 -0800

    Using Trajectory mode to move ODrive to specified location

--- main.py
+++ main.py
@@ -11,26 +11,22 @@ OD = odrive.find_any()
 axis = ODrive_Ease_Lib.ODrive_Axis(OD.axis1)
 
 
-def run_odrive_with_joystick():
-    joystick.get_axis(0)
-
-
 if __name__ == '__main__':
     axis.clear_errors()
     axis.calibrate()
     axis.home_with_vel(-6000, -112844.0)
-    middle = (axis.zero - axis.get_pos())/2
+    print(axis.zero)
+    print(axis.get_pos())
+    middle = (0 - axis.get_pos())/2
 
-    print(middle)
-
-    full_length = 112814.0
+    full_length = 112816
 
     # god tier code to switch variables if needed
     # right_end = right_end ^ left_end
     # left_end = right_end ^ left_end
     # right_end = right_end ^ left_end
 
-    axis.set_vel_limit(25000)
+    axis.set_vel_limit(27000)
 
     # axis.set_pos_gain(30)
     # axis.set_vel_gain(.0002)
@@ -43,19 +39,19 @@ if __name__ == '__main__':
     try:
         while True:
 
-            pos = axis.get_raw_pos()
+            pos = axis.get_pos()
 
-            if pos > -(full_length-1000) and joystick.get_axis('x') > .15:
+            if pos > -(full_length-4000) and joystick.get_axis('x') > .15:
                 if switch:
                     print("right")
                     switch = False
-                axis.set_vel(-18000*joystick.get_axis('x'))
+                axis.set_vel(-22000*joystick.get_axis('x'))
                 # axis.set_pos(pos-(1000*joystick.get_axis('x')))
-            elif pos < (axis.zero + 1000) and joystick.get_axis('x') < -.15:
+            elif pos < -4000 and joystick.get_axis('x') < -.15:
                 if not switch:
                     print("left")
                     switch = True
-                axis.set_vel(18000*-joystick.get_axis('x'))
+                axis.set_vel(22000*-joystick.get_axis('x'))
                 # axis.set_pos(pos + (1000*-joystick.get_axis('x')))
             else:
                 axis.set_vel(0)
@@ -69,7 +65,10 @@ if __name__ == '__main__':
 
             if joystick.button_combo_check([3]):
                 print("Oof")
-                axis.set_pos_trap(-middle)
+                while abs(axis.get_pos() - -middle) >= 50:
+                    axis.set_pos_trap(-middle)
+                    axis.get_pos()
+                    print("tracking...")
 
 
 
