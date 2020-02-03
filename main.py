26d947c0938c333c552ec415f02b936bb4d8d901
Chris Schniepp <chrisschniepp@dpengineering.org>
2020-02-03 13:57:07 -0800

    working on two odrives

--- main.py
+++ main.py
@@ -7,18 +7,23 @@ from pidev.Joystick import Joystick
 
 joystick = Joystick(0, False)
 
+
 OD = odrive.find_any()
-axis = ODrive_Ease_Lib.ODrive_Axis(OD.axis1)
+axis_1 = ODrive_Ease_Lib.ODrive_Axis(OD.axis1)
+axis_0 = ODrive_Ease_Lib.ODrive_Axis(OD.axis0)
+both_axises = ODrive_Ease_Lib.double_ODrive(OD.axis1, OD.axis0)
 
 
 if __name__ == '__main__':
-    axis.clear_errors()
-    axis.set_curr_limit(30)
-    axis.calibrate()
-    axis.home_with_vel(-10000, -112844.0)
-    print(axis.zero)
-    print(axis.get_pos())
-    middle = (0 - axis.get_pos())/2
+    axis_1.clear_errors()
+    axis_0.clear_errors()
+    axis_1.set_curr_limit(30)
+    axis_0.set_curr_limit(30)
+    both_axises.calibrate()
+    both_axises.home_with_vel(-10000, -112844.0)
+    print(axis_1.zero, " ", axis_0.zero)
+    print(axis_1.get_pos(), " ", axis_0.get_pos())
+    middle = (0 - axis_1.get_pos())/2
 
     full_length = 112816
 
@@ -27,47 +32,54 @@ if __name__ == '__main__':
     # left_end = right_end ^ left_end
     # right_end = right_end ^ left_end
 
-    axis.set_vel_limit(250000)
+    axis_1.set_vel_limit(250000)
+    axis_0.set_vel_limit(250000)
+    vel_speed = 50000
 
-    # axis.set_pos_gain(30)
-    # axis.set_vel_gain(.0002)
+    # axis_1.set_pos_gain(30)
+    # axis_1.set_vel_gain(.0002)
 
     switch = True
 
     try:
         while True:
 
-            pos = axis.get_pos()
+            pos = axis_1.get_pos()
+            pos1 = axis_0.get_pos()
 
             if pos > -(full_length-15000) and joystick.get_axis('x') > .15:
                 if switch:
                     print("right")
                     switch = False
-                axis.set_vel(-225000*joystick.get_axis('x'))
-                # axis.set_pos(pos-(1000*joystick.get_axis('x')))
+                axis_1.set_vel(-vel_speed*joystick.get_axis('x'))
+                axis_0.set_vel(-vel_speed*joystick.get_axis('y'))
+                # axis_1.set_pos(pos-(1000*joystick.get_axis_1('x')))
             elif pos < -15000 and joystick.get_axis('x') < -.15:
                 if not switch:
                     print("left")
                     switch = True
-                axis.set_vel(225000*-joystick.get_axis('x'))
-                # axis.set_pos(pos + (1000*-joystick.get_axis('x')))
+                axis_1.set_vel(vel_speed*-joystick.get_axis('x'))
+                axis_0.set_vel(vel_speed * joystick.get_axis('y'))
+                # axis_1.set_pos(pos + (1000*-joystick.get_axis_1('x')))
             else:
-                axis.set_vel(0)
+                axis_1.set_vel(0)
+                axis_0.set_vel(0)
 
             if joystick.button_combo_check([6]):
-                print("position: ", axis.get_pos())
-                print("velocity: ", axis.get_vel())
+                print("position: ", axis_1.get_pos(), " y: ", axis_0.get_pos())
+                print("velocity: ", axis_1.get_vel(), "y: ", axis_0.get_vel())
                 sleep(.2)
 
             if joystick.button_combo_check([3]):
                 print("Oof")
-                while abs(axis.get_pos() - -middle) >= 50:
-                    axis.set_pos_trap(-middle)
-                    axis.get_pos()
+                while abs(axis_1.get_pos() - -middle) >= 50:
+                    axis_1.set_pos_trap(-middle)
+                    axis_1.get_pos()
                     print("tracking...")
 
 
 
     except KeyboardInterrupt:
-        axis.idle()
+        axis_1.idle()
+        axis_0.idle()
         quit()
