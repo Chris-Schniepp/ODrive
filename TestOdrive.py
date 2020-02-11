64acf81c97e44acf2d1bd168125e76ecc5f57523
Chris Schniepp <chrisschniepp@dpengineering.org>
2020-02-11 13:55:30 -0800

    working with two axises

--- TestOdrive.py
+++ TestOdrive.py
@@ -8,21 +8,21 @@ from pidev.Joystick import Joystick
 joystick = Joystick(0, False)
 
 OD = odrive.find_any()
-axis = ODrive_Ease_Lib.ODrive_Axis(OD.axis1)
+axis = ODrive_Ease_Lib.ODrive_Axis(OD.axis0)
 
 
 if __name__ == '__main__':
     axis.clear_errors()
     axis.calibrate()
-    axis.home_with_vel(-5000, -1.0)
+   # axis.home_with_vel(-5000, -1.0)
     print(axis.zero)
-    print(axis.get_pos())
+    print(axis.get_pos(), " ", axis.get_raw_pos())
     middle = (0 - axis.get_pos())/2
 
     full_length = 112816
     restricted_length = 1
 
-    axis.set_vel_limit(250000)
+    axis.set_vel_limit(50000)
 
     switch = True
 
@@ -35,19 +35,19 @@ if __name__ == '__main__':
                 if switch:
                     print("right")
                     switch = False
-                axis.set_vel(50000*joystick.get_axis('y'))
+                axis.set_vel(10000*joystick.get_axis('y'))
 
             elif pos < -restricted_length and joystick.get_axis('y') > .15:
                 if not switch:
                     print("left")
                     switch = True
-                axis.set_vel(50000*joystick.get_axis('y'))
+                axis.set_vel(10000*joystick.get_axis('y'))
 
             else:
                 axis.set_vel(0)
 
             if joystick.button_combo_check([6]):
-                print("position: ", axis.get_pos())
+                print("position: ", axis.get_raw_pos())
                 print("velocity: ", axis.get_vel())
                 sleep(.2)
 
